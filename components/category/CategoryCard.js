c6c20c9337f78afd6a92dd6be8ab666228944278
Sebastian Villegas <juan.villega@mercadolibre.com.co>
2022-06-14 19:33:01 -0500

    Fix category cards

--- components/category/CategoryCard.js
+++ components/category/CategoryCard.js
@@ -1,13 +1,9 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
-import { Card, Grid, Text, Link } from '@nextui-org/react'
+import { Card, Grid, Text } from '@nextui-org/react'
 
-export const CategoryCard = ({
-  category,
-  clickable = true,
-  margin = true,
-}) => {
+export const CategoryCard = ({ category, clickable = true, margin = true }) => {
   const router = useRouter()
   const { category_id, name, icon_url, description, total_podcasts } = category
   return (
@@ -16,36 +12,42 @@ export const CategoryCard = ({
       isPressable={clickable}
       isHoverable
       onPress={() => router.push(`/dashboard/categories/${category_id}`)}
+      onClick={() => router.push(`/dashboard/categories/${category_id}`)}
     >
       <Card.Header>
-        <Image
-          alt='nextui logo'
-          onError={(e) =>
-            3(
-              (e.target.src =
-                'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png')
-            )
-          }
-          src={
-            icon_url
-              ? icon_url
-              : 'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png'
-          }
-          width='50px'
-          height='50px'
-        />
-        <Grid.Container css={{ pl: '$6' }}>
-          <Grid xs={12}>
+        <Grid.Container gap={1.5}>
+          <Grid xs={12} md={3} justify='center'>
+            <Image
+              alt='nextui logo'
+              objectFit='cover'
+              width={100}
+              height={100}
+              onError={(e) =>
+                (e.target.src =
+                  'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png')
+              }
+              src={
+                icon_url
+                  ? icon_url
+                  : 'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png'
+              }
+            />
+          </Grid>
+          <Grid
+            xs={12}
+            md={9}
+            direction='column'
+            justify='center'
+            css={{ '@mdMax': { textAlign: 'center' } }}
+          >
             <Text h4 css={{ lineHeight: '$xs' }}>
               {name}
             </Text>
-          </Grid>
-          <Grid xs={12}>
             <Text css={{ color: '$accents8' }}>{total_podcasts} Podcasts</Text>
           </Grid>
         </Grid.Container>
       </Card.Header>
-      <Card.Body css={{ py: '$2' }}>
+      <Card.Body css={{ py: '$2', '@mdMax': { textAlign: 'center' } }}>
         <Text>{description}</Text>
       </Card.Body>
     </Card>
