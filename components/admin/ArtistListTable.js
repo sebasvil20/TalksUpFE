e97de6d3867cf4e1126994a8a5b42358c7ca4093
Sebastian Villegas <juan.villega@mercadolibre.com.co>
2022-06-24 11:29:35 -0500

    Add custom pagination to tables to handle bug

--- components/admin/ArtistListTable.js
+++ components/admin/ArtistListTable.js
@@ -1,104 +1,133 @@
-import { Table, Row, Col, User, Text } from '@nextui-org/react'
+import { useEffect, useState } from 'react'
+import {
+  Table,
+  Row,
+  Col,
+  User,
+  Text,
+  Pagination,
+  Grid,
+  Spacer,
+} from '@nextui-org/react'
 
-import { DeleteConfirmationModal, UpgradeToAdminConfirmationModal } from './'
+import { DeleteConfirmationModal } from './'
 
 export const ArtistListTable = ({ artists, fetchData }) => {
   const columns = [
     { name: 'ARTIST', uid: 'artist' },
     { name: 'PODCASTS', uid: 'podcasts' },
-    { name: 'ACTIONS', uid: 'actions' },
+    { name: 'ACTIONS', uid: 'actions2' },
   ]
 
+  const [totalPages, setTotalPages] = useState(Math.ceil(artists.length / 5))
+  const [currentPage, setCurrentPage] = useState(1)
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(artists.length / 5))
+  }, [artists])
+
+  console.log(totalPages)
   return (
-    <Table
-      aria-label='Authors table'
-      css={{
-        height: 'auto',
-        minWidth: '100%',
-        width: '100%',
-        zIndex: '1!important',
-      }}
-      selectionMode='none'
-    >
-      <Table.Header columns={columns}>
-        {(column) => (
-          <Table.Column
-            key={column.uid}
-            hideHeader={column.uid === 'actions'}
-            align={column.uid === 'actions' ? 'center' : 'start'}
-          >
-            {column.name}
-          </Table.Column>
-        )}
-      </Table.Header>
-      <Table.Body
-        items={artists}
+    <>
+      <Table
+        aria-label='Authors table'
         css={{
+          height: 'auto',
+          minWidth: '100%',
+          width: '100%',
           zIndex: '1!important',
         }}
+        selectionMode='none'
       >
-        {artists.map((artist) => (
-          <Table.Row
-            key={artist.artist_id}
-            css={{
-              zIndex: '1!important',
-            }}
-          >
-            <Table.Cell
-              css={{
-                zIndex: '1!important',
-              }}
+        <Table.Header columns={columns}>
+          {(column) => (
+            <Table.Column
+              key={column.uid}
+              hideHeader={column.uid === 'actions2'}
+              align={column.uid === 'actions2' ? 'center' : 'start'}
             >
-              <User
-                squared
-                onError={(e) =>
-                  (e.target.src =
-                    'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png')
-                }
-                src={
-                    artist.profile_pic_url
-                    ? artist.profile_pic_url
-                    : 'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png'
-                }
-                name={`${artist.name}`}
-                css={{ p: 0 }}
+              {column.name}
+            </Table.Column>
+          )}
+        </Table.Header>
+        <Table.Body
+          items={artists.slice(
+            (currentPage - 1) * 5,
+            (currentPage - 1) * 5 + 5
+          )}
+          css={{
+            zIndex: '1!important',
+          }}
+        >
+          {artists
+            .slice((currentPage - 1) * 5, (currentPage - 1) * 5 + 5)
+            .map((artist) => (
+              <Table.Row
+                key={artist.author_id}
+                css={{
+                  zIndex: '1!important',
+                }}
               >
-                {artist.name}
-              </User>
-            </Table.Cell>
-            <Table.Cell>
-              <Col>
-                <Row>
-                  <Text
-                    b
-                    size={14}
-                    css={{ tt: 'capitalize' }}
-                  >{artist.total_podcasts}
-                  </Text>
-                </Row>
-              </Col>
-            </Table.Cell>
-            <Table.Cell>
-              <Row justify='center' align='center'>
-                <Col css={{ d: 'flex' }}>
-                  <DeleteConfirmationModal
-                    user_id={artist.artist_id}
-                    public_name={artist.name}
-                    fetchData={fetchData}
-                  />
-                </Col>
-              </Row>
-            </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-      <Table.Pagination
-        shadow
-        noMargin
-        align='center'
-        rowsPerPage={5}
-        onPageChange={(page) => console.log({ page })}
-      />
-    </Table>
+                <Table.Cell
+                  css={{
+                    zIndex: '1!important',
+                  }}
+                >
+                  <User
+                    squared
+                    onError={(e) =>
+                      (e.target.src =
+                        'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png')
+                    }
+                    src={
+                      artist.profile_pic_url
+                        ? artist.profile_pic_url
+                        : 'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png'
+                    }
+                    name={`${artist.name}`}
+                    css={{ p: 0 }}
+                  >
+                    {artist.name}
+                  </User>
+                </Table.Cell>
+                <Table.Cell>
+                  <Col>
+                    <Row>
+                      <Text b size={14} css={{ tt: 'capitalize' }}>
+                        {artist.total_podcasts}
+                      </Text>
+                    </Row>
+                  </Col>
+                </Table.Cell>
+                <Table.Cell>
+                  <Row justify='center' align='center'>
+                    <Col css={{ d: 'flex' }}>
+                      <DeleteConfirmationModal
+                        user_id={artist.artist_id}
+                        public_name={artist.name}
+                        fetchData={fetchData}
+                      />
+                    </Col>
+                  </Row>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+        </Table.Body>
+      </Table>
+      {totalPages > 1 && (
+        <Grid.Container>
+          <Spacer />
+          <Grid xs={12} justify='center'>
+            <Pagination
+              shadow
+              color='secondary'
+              page={currentPage}
+              onChange={(page) => setCurrentPage(page)}
+              total={totalPages}
+            />
+          </Grid>
+        </Grid.Container>
+      )}
+    </>
   )
 }
--- components/admin/UserListTable.js
+++ components/admin/UserListTable.js
@@ -1,4 +1,15 @@
-import { Table, Row, Col, User, Text } from '@nextui-org/react'
+import { useEffect, useState } from 'react'
+
+import {
+  Table,
+  Row,
+  Col,
+  User,
+  Text,
+  Grid,
+  Spacer,
+  Pagination,
+} from '@nextui-org/react'
 
 import { DeleteConfirmationModal, UpgradeToAdminConfirmationModal } from './'
 
@@ -9,108 +20,124 @@ export const UserListTable = ({ users, fetchData }) => {
     { name: 'ACTIONS', uid: 'actions' },
   ]
 
+  const [totalPages, setTotalPages] = useState(Math.ceil(users.length / 5))
+  const [currentPage, setCurrentPage] = useState(1)
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(users.length / 5))
+  }, [users])
+
   return (
-    <Table
-      aria-label='Users table'
-      css={{
-        height: 'auto',
-        minWidth: '100%',
-        width: '100%',
-        zIndex: '1!important',
-      }}
-      selectionMode='none'
-    >
-      <Table.Header columns={columns}>
-        {(column) => (
-          <Table.Column
-            key={column.uid}
-            hideHeader={column.uid === 'actions'}
-            align={column.uid === 'actions' ? 'center' : 'start'}
-          >
-            {column.name}
-          </Table.Column>
-        )}
-      </Table.Header>
-      <Table.Body
-        items={users}
+    <>
+      <Table
+        aria-label='Users table'
         css={{
+          height: 'auto',
+          minWidth: '100%',
+          width: '100%',
           zIndex: '1!important',
         }}
+        selectionMode='none'
       >
-        {users.map((user) => (
-          <Table.Row
-            key={user.user_id}
-            css={{
-              zIndex: '1!important',
-            }}
-          >
-            <Table.Cell
+        <Table.Header columns={columns}>
+          {(column) => (
+            <Table.Column
+              key={column.uid}
+              hideHeader={column.uid === 'actions'}
+              align={column.uid === 'actions' ? 'center' : 'start'}
+            >
+              {column.name}
+            </Table.Column>
+          )}
+        </Table.Header>
+        <Table.Body
+          items={users}
+          css={{
+            zIndex: '1!important',
+          }}
+        >
+          {users.map((user) => (
+            <Table.Row
+              key={user.user_id}
               css={{
                 zIndex: '1!important',
               }}
             >
-              <User
-                squared
-                onError={(e) =>
-                  (e.target.src =
-                    'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png')
-                }
-                src={
-                  user.profile_pic_url
-                    ? user.profile_pic_url
-                    : 'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png'
-                }
-                name={`${user.first_name} ${user.last_name}`}
-                css={{ p: 0 }}
+              <Table.Cell
+                css={{
+                  zIndex: '1!important',
+                }}
               >
-                {user.email}
-              </User>
-            </Table.Cell>
-            <Table.Cell>
-              <Col>
-                <Row>
-                  <Text
-                    b
-                    size={14}
-                    css={{ tt: 'capitalize' }}
-                    color={user.role == '1' && '#6333eb'}
-                  >
-                    {user.role == '1' ? 'Admin 👮🏻‍♀️' : 'User'}
-                  </Text>
-                </Row>
-              </Col>
-            </Table.Cell>
-            <Table.Cell>
-              <Row justify='center' align='center'>
-                <Col css={{ d: 'flex' }}>
-                  <UpgradeToAdminConfirmationModal
-                    user_id={user.user_id}
-                    public_name={user.public_name}
-                    role={user.role}
-                    fetchData={fetchData}
-                  />
+                <User
+                  squared
+                  onError={(e) =>
+                    (e.target.src =
+                      'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png')
+                  }
+                  src={
+                    user.profile_pic_url
+                      ? user.profile_pic_url
+                      : 'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png'
+                  }
+                  name={`${user.first_name} ${user.last_name}`}
+                  css={{ p: 0 }}
+                >
+                  {user.email}
+                </User>
+              </Table.Cell>
+              <Table.Cell>
+                <Col>
+                  <Row>
+                    <Text
+                      b
+                      size={14}
+                      css={{ tt: 'capitalize' }}
+                      color={user.role == '1' && '#6333eb'}
+                    >
+                      {user.role == '1' ? 'Admin 👮🏻‍♀️' : 'User'}
+                    </Text>
+                  </Row>
                 </Col>
-                {user.role != 1 && (
+              </Table.Cell>
+              <Table.Cell>
+                <Row justify='center' align='center'>
                   <Col css={{ d: 'flex' }}>
-                    <DeleteConfirmationModal
+                    <UpgradeToAdminConfirmationModal
                       user_id={user.user_id}
                       public_name={user.public_name}
+                      role={user.role}
                       fetchData={fetchData}
                     />
                   </Col>
-                )}
-              </Row>
-            </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-      <Table.Pagination
-        shadow
-        noMargin
-        align='center'
-        rowsPerPage={5}
-        onPageChange={(page) => console.log({ page })}
-      />
-    </Table>
+                  {user.role != 1 && (
+                    <Col css={{ d: 'flex' }}>
+                      <DeleteConfirmationModal
+                        user_id={user.user_id}
+                        public_name={user.public_name}
+                        fetchData={fetchData}
+                      />
+                    </Col>
+                  )}
+                </Row>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+      {totalPages > 1 && (
+        <Grid.Container>
+          <Spacer />
+          <Grid xs={12} justify='center'>
+            <Pagination
+              shadow
+              color='secondary'
+              page={currentPage}
+              onChange={(page) => setCurrentPage(page)}
+              total={totalPages}
+            />
+          </Grid>
+        </Grid.Container>
+      )}
+    </>
   )
 }
