6ff54160151eadc1184f3e6fef43f4de509cbc49
Sebastian Villegas <juan.villega@mercadolibre.com.co>
2022-06-24 19:02:03 -0500

    Fix user table

--- components/admin/UserListTable.js
+++ components/admin/UserListTable.js
@@ -11,7 +11,10 @@ import {
   Pagination,
 } from '@nextui-org/react'
 
-import { DeleteUserConfirmationModal, UpgradeToAdminConfirmationModal } from './'
+import {
+  DeleteUserConfirmationModal,
+  UpgradeToAdminConfirmationModal,
+} from './'
 
 export const UserListTable = ({ users, fetchData }) => {
   const columns = [
@@ -56,72 +59,74 @@ export const UserListTable = ({ users, fetchData }) => {
             zIndex: '1!important',
           }}
         >
-          {users.map((user) => (
-            <Table.Row
-              key={user.user_id}
-              css={{
-                zIndex: '1!important',
-              }}
-            >
-              <Table.Cell
+          {users
+            .slice((currentPage - 1) * 5, (currentPage - 1) * 5 + 5)
+            .map((user) => (
+              <Table.Row
+                key={user.user_id}
                 css={{
                   zIndex: '1!important',
                 }}
               >
-                <User
-                  squared
-                  onError={(e) =>
-                    (e.target.src =
-                      'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png')
-                  }
-                  src={
-                    user.profile_pic_url
-                      ? user.profile_pic_url
-                      : 'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png'
-                  }
-                  name={`${user.first_name} ${user.last_name}`}
-                  css={{ p: 0 }}
+                <Table.Cell
+                  css={{
+                    zIndex: '1!important',
+                  }}
                 >
-                  {user.email}
-                </User>
-              </Table.Cell>
-              <Table.Cell>
-                <Col>
-                  <Row>
-                    <Text
-                      b
-                      size={14}
-                      css={{ tt: 'capitalize' }}
-                      color={user.role == '1' && '#6333eb'}
-                    >
-                      {user.role == '1' ? 'Admin 👮🏻‍♀️' : 'User'}
-                    </Text>
-                  </Row>
-                </Col>
-              </Table.Cell>
-              <Table.Cell>
-                <Row justify='center' align='center'>
-                  <Col css={{ d: 'flex' }}>
-                    <UpgradeToAdminConfirmationModal
-                      user_id={user.user_id}
-                      public_name={user.public_name}
-                      role={user.role}
-                      fetchData={fetchData}
-                    />
+                  <User
+                    squared
+                    onError={(e) =>
+                      (e.target.src =
+                        'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png')
+                    }
+                    src={
+                      user.profile_pic_url
+                        ? user.profile_pic_url
+                        : 'https://talksupcdn.sfo3.cdn.digitaloceanspaces.com/88be4dd4-dc7b-11ec-b799-acde48001122.png'
+                    }
+                    name={`${user.first_name} ${user.last_name}`}
+                    css={{ p: 0 }}
+                  >
+                    {user.email}
+                  </User>
+                </Table.Cell>
+                <Table.Cell>
+                  <Col>
+                    <Row>
+                      <Text
+                        b
+                        size={14}
+                        css={{ tt: 'capitalize' }}
+                        color={user.role == '1' && '#6333eb'}
+                      >
+                        {user.role == '1' ? 'Admin 👮🏻‍♀️' : 'User'}
+                      </Text>
+                    </Row>
                   </Col>
-                  {user.role != 1 && (
+                </Table.Cell>
+                <Table.Cell>
+                  <Row justify='center' align='center'>
                     <Col css={{ d: 'flex' }}>
-                      <DeleteUserConfirmationModal
+                      <UpgradeToAdminConfirmationModal
                         user_id={user.user_id}
                         public_name={user.public_name}
+                        role={user.role}
                         fetchData={fetchData}
                       />
                     </Col>
-                  )}
-                </Row>
-              </Table.Cell>
-            </Table.Row>
-          ))}
+                    {user.role != 1 && (
+                      <Col css={{ d: 'flex' }}>
+                        <DeleteUserConfirmationModal
+                          user_id={user.user_id}
+                          public_name={user.public_name}
+                          fetchData={fetchData}
+                        />
+                      </Col>
+                    )}
+                  </Row>
+                </Table.Cell>
+              </Table.Row>
+            ))}
         </Table.Body>
       </Table>
       {totalPages > 1 && (
