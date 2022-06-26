76ba4a2dd0096be4abf14b2b0e9cee06c8f82567
Sebastian Villegas <juan.villega@mercadolibre.com.co>
2022-06-26 13:03:11 -0500

    Add padding to menu links

--- components/podcast/DashboardLink.js
+++ components/podcast/DashboardLink.js
@@ -4,6 +4,7 @@ export const MenuLink = ({ text, isActive, onClickFunc }) => {
   const linkStyle = {
     color: isActive ? '#6334EB' : '#A0A3BD',
     cursor: 'pointer',
+    padding: '10px 0',
     '@sm': {
       marginRight: '40px',
     },
